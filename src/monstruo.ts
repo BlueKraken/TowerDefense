1aab1f8d029286c0459d176804e10465de3ce9ec
Bruno Zerega <brunozerega@gmail.com>
2018-12-29 12:37:12 -0300

    Estructura de monstruo

--- src/monstruo.ts
+++ src/monstruo.ts
@@ -1,3 +1,18 @@
 class Monstruo {
-    
+    private _velocidad: number;
+    private _vida: number;
+    private _posicion: number; //posiblemente Punto 
+
+    constructor() {
+
+    }
+
+    mover() {
+
+    }
+
+    morir() {
+
+    }
 }
+
