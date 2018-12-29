5352367b42d71c021676d32a3560f79d18f8fed5
Bruno Zerega <brunozerega@gmail.com>
2018-12-29 20:37:55 -0300

    update a repositorio. no hace nada

--- src/monstruo.ts
+++ src/monstruo.ts
@@ -1,17 +1,24 @@
+import { Punto } from "./punto";
+
 class Monstruo {
-    private _velocidad: number;
+    private _velocidad: number; //1 celda por ciclo
     private _vida: number;
-    private _posicion: number; //posiblemente Punto 
+    private _posicion: Punto;
+    //camino
 
     constructor() {
 
     }
 
-    mover() {
+    mover() { //este es tu camino voh siguelo
 
     }
 
     morir() {
+
+    }
+
+    recibirDanio() {
 
     }
 }
