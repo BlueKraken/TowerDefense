3d9146d3867689c7adc959740d9ff67906d488a3
Bluekraken <gonzalo.tajmuch@gmail.com>
2019-01-12 11:53:27 -0300

    Agrega clase escena responsable de dibujar el juego

---
+++ src/escena.ts
@@ -0,0 +1,16 @@
+import { Monstruo } from "./monstruo";
+import { Torre } from "./torre";
+
+export class Escena {
+    public dibujarCamino(camino: number[][]): void {
+
+    }
+
+    public dibujarMonstruo(monstruo: Monstruo): void {
+
+    }
+
+    public dibujarTorre(torre: Torre): void {
+
+    }
+}
