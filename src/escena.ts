7a6a723e69916fde7f4f9f4230e7f670486157b5
Bluekraken <gonzalo.tajmuch@gmail.com>
2019-01-12 12:07:59 -0300

    Agrega mecanismo de mapeo de punto a posicion fisica para clase Escena

--- src/escena.ts
+++ src/escena.ts
@@ -1,5 +1,6 @@
 import { Monstruo } from "./monstruo";
 import { Torre } from "./torre";
+import { Punto } from "./punto";
 
 export class Escena {
     public dibujarCamino(camino: number[][]): void {
@@ -12,5 +13,12 @@ export class Escena {
 
     public dibujarTorre(torre: Torre): void {
 
+    }
+
+    private coordenadasAPosicion(punto: Punto) {
+        return {
+            left: punto.x * 10,
+            top: punto.y * 10
+        }
     }
 }
