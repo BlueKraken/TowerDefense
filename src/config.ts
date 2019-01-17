1aa2fc9499b303b6af1f20a0a50670a086e55775
Bluekraken <gonzalo.tajmuch@gmail.com>
2019-01-17 20:35:14 -0300

    Cambia vida de monstruos en oleadas a 1 dígito

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const gameConfig = {
     intervalo: 1000,
     oleadas: [
-        { velocidad: 2, vida: 10, cantidad: 10 },
-        { velocidad: 3, vida: 12, cantidad: 12 }
+        { velocidad: 2, vida: 7, cantidad: 10 },
+        { velocidad: 3, vida: 9, cantidad: 12 }
     ] as Oleada[]
 }
 
