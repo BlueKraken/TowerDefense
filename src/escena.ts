578bb60588ccaf1ab908ec558209a538e2167400
Bluekraken <gonzalo.tajmuch@gmail.com>
2019-01-19 17:11:50 -0300

    implementacion completa de dibujado. Falta probar

--- src/escena.ts
+++ src/escena.ts
@@ -2,31 +2,52 @@ import { Monstruo } from "./monstruo";
 import { Torre } from "./torre";
 
 export class Escena {
+
+    public constructor() {
+        this._contenedor = document.createElement('div');
+        document.addEventListener('DOMContentLoaded', () => {
+            document.body.innerHTML = '';
+            document.body.append(this._contenedor);
+        });
+    }
+
+    private _contenedor: HTMLDivElement;
+    
     public dibujarEscena(camino: number[][], monstruos: Monstruo[], torres: Torre[]) 
     {
-
+        // como refactorizar esto?
+        this.imprimirEscena(
+            this.dibujarMonstruos(
+                this.dibujarTorres(
+                    this.dibujarCamino(camino), torres), monstruos));
     }
     
-    public dibujarCamino(camino: number[][]): string[][] {
+    private dibujarCamino(camino: number[][]): string[][] {
         const parse: ToAscii = x => x == 0 ? '#' : ' ';
 
         return camino.map(x => x.map(parse));
     }
 
     private dibujarTorres(tablero: string[][], torres: Torre[]): string[][] {
-        const torreToAcci: ToAscii = x => 'T';
-
         let nuevoTablero = [...tablero];
+        torres.forEach(t => nuevoTablero[t.posicion.x][t.posicion.y] == 't');
 
-        torres.forEach(t => nuevoTablero[]) nuevoTablero
+        return nuevoTablero;
     }
 
-    private dibujarMonstruos(tablero: string[][], monstruos: Monstruo[]): string[] {
+    private dibujarMonstruos(tablero: string[][], monstruos: Monstruo[]): string[][] {
+        let nuevoTablero = [...tablero];
 
+        monstruos.forEach(
+            m => nuevoTablero[m.posicion.x][m.posicion.y] = m.vida.toString()); // justo 80 owo
+
+        return nuevoTablero;
     }
 
     private imprimirEscena(tablero: string[][]): void {
-
+        document.addEventListener('DOMContentLoaded', () => {
+            this._contenedor.innerText = tablero.map(fila => fila.join('')).join('\n')
+        });
     }
 }
 
