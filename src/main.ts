7df026ce997260a43715ecc946c08489cd914845
bzerega <brunozerega@gmail.com>
2019-01-28 20:22:10 -0300

    Inicializa el juego

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,25 @@ import { Torre } from "./torre";
 import { Punto } from "./punto";
 import { TipoAtaque } from "./tipoAtaque";
 import { Monstruo } from "./monstruo";
+import { Juego } from "./juego";
 
-const e = new Escena();
+
+
+/*const e = new Escena();
 
 (window as any).escena = e;
 
-const camino = [
+*/
+const mapa = [
     [1,2,3,4],
     [0,0,0,5],
     [9,8,7,6]
-]
+];
 
-const caminoDePuntos = []
+let juego = new Juego(mapa);
+
+/*
+const caminoDePuntos = [];
 for (let i = 0; i < camino.length; i++) {
     for (let j = 0; j < camino[i].length; j++) {
         const element = camino[i][j]
@@ -26,6 +33,7 @@ for (let i = 0; i < camino.length; i++) {
 
 const torre = new Torre(new Punto(1,1), 3, new TipoAtaque(5, 444));
 
-const monstruo = new Monstruo(4, 4, caminoDePuntos);
+const monstruo = new Monstruo(1, 4, caminoDePuntos);
 
-e.dibujarEscena(camino, [monstruo], [torre]);
+e.dibujarEscena(camino, [monstruo], [torre]);*/
+
