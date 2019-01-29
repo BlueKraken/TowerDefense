dafcc3afb54fc494e6b8b763e367afa70cbbc806
Bluekraken <gonzalo.tajmuch@gmail.com>
2019-01-28 22:21:56 -0300

    arregla main

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,10 @@
 import { Juego } from "./juego";
-import a from 'sweetalert';
 
+const mapa = [
+    [1,2,3,4],
+    [0,0,0,5],
+    [9,8,7,6]
+];
 
-a('eeee');
-
-/*const e = new Escena();
-
-(window as any).escena = e;
-
-*/
-// const mapa = [
-//     [1,2,3,4],
-//     [0,0,0,5],
-//     [9,8,7,6]
-// ];
-
-// let juego = new Juego(mapa);
-
-/*
-const caminoDePuntos = [];
-for (let i = 0; i < camino.length; i++) {
-    for (let j = 0; j < camino[i].length; j++) {
-        const element = camino[i][j]
-        if (element != 0) {
-            caminoDePuntos.push(new Punto(i, j));
-        } 
-    }
-}
-
-const torre = new Torre(new Punto(1,1), 3, new TipoAtaque(5, 444));
-
-const monstruo = new Monstruo(1, 4, caminoDePuntos);
-
-e.dibujarEscena(camino, [monstruo], [torre]);*/
+let juego = new Juego(mapa);
 
